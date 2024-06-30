5dd6c44ee1fd9e54c8c01851e686dd1d45e0d1bd
Oleksandr Zhuravsky <zhuravsky31@gmail.com>
2024-06-30 22:29:12 +0300

    add docs for contacts operations

--- src/constants/index.js
+++ src/constants/index.js
@@ -11,3 +11,5 @@ export const THIRTY_DAYS = 30 * 24 * 60 * 60 * 1000;
 
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 export const UPLOADS_DIR = path.join(process.cwd(), 'uploads');
+
+export const SWAGGER_JSON = path.join(process.cwd(), 'docs', 'swagger.json');
